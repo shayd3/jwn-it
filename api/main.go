e373fd289efd403c91b4569f4f7f24d34b18d44d
shayd3 <ryan.ross215@gmail.com>
2020-10-18 09:27:38 -0400

    Testing gin parameters

--- api/main.go
+++ api/main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Creates a gin router with default middleware
 	// logger and recovery (crash-free) middleware
-	r := gin.Default()
+	router := gin.Default()
 
 
-	r.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H {
 			"message": "pong",
 		})
 	})
+	
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+
+	})
+
 
 	// By default, it serves on :8080 unless
 	// a PORT environment variable was defined
-	r.Run()
+	router.Run()
 	// router.Run(":3000") for a hard coded port
 }
