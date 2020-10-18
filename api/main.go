c3207f39e76b734c9bca65928a139e7a20af2d27
shayd3 <ryan.ross215@gmail.com>
2020-10-18 15:29:30 -0400

    Setting up bolt db with a test bucket creation

--- api/main.go
+++ api/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	bolt "go.etcd.io/bbolt"
 )
 
 func main() {
+	// set up bboltdb
+	// Open my.db data file in current dir
+	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create bucket
+	db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("TestBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		log.Printf("Bucket Depth: %v", bucket.Stats().Depth) 
+		return nil
+	})
+
+
 	// Creates a gin router with default middleware
 	// logger and recovery (crash-free) middleware
 	router := gin.Default()
