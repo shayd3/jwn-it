72d70b8031b86a3a6dfb94320bc371dd899b7b22
shayd3 <ryan.ross@temple.edu>
2021-01-02 08:10:46 -0500

    Refactor create endpoint

--- controllers/jwnit.go
+++ controllers/jwnit.go
@@ -3,15 +3,19 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/shayd3/jwn-it/data"
 	"github.com/shayd3/jwn-it/models"
 	bolt "go.etcd.io/bbolt"
 )
 
 // GetURLEntries returns a list of URLEntry objects.
-func GetURLEntries(db *bolt.DB) ([]models.URLEntry, error) {
-	urlEntrys := []models.URLEntry{}
-	err := db.View(func(t *bolt.Tx) error {
+func GetURLEntries(c *gin.Context) {
+	urlEntries := []models.URLEntry{}
+	err := data.DB.View(func(t *bolt.Tx) error {
 		bucket := t.Bucket([]byte("JWNIT"))
 		bucket.ForEach(func(k, v []byte) error {
 			var urlEntry models.URLEntry
@@ -19,28 +23,51 @@ func GetURLEntries(db *bolt.DB) ([]models.URLEntry, error) {
 			if err != nil {
 				return err
 			}
-			urlEntrys = append(urlEntrys, urlEntry)
+			urlEntries = append(urlEntries, urlEntry)
 			return nil
 		})
 		return nil
 	})
-	return urlEntrys, err
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H {
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H {
+			"data": urlEntries,
+			"error": "",
+		})
+	}
 }
 
 // AddURLEntry adds an URLEntry to the db. 
-func AddURLEntry(db *bolt.DB, entry models.URLEntry) error {
-	err := db.Update(func(t *bolt.Tx) error {
-		encoded, err := json.Marshal(entry)
+func AddURLEntry(c *gin.Context) {
+	urlEntry := models.URLEntry{}
+	err := c.BindJSON(&urlEntry)
+	urlEntry.Created = time.Now()
+
+	err = data.DB.Update(func(t *bolt.Tx) error {
+		encoded, err := json.Marshal(urlEntry)
 		if err != nil {
 			return fmt.Errorf("could not marshall URLEntry object: %v", err)
 		}
-		err = t.Bucket([]byte("JWNIT")).Put([]byte(entry.Slug), encoded)
+		err = t.Bucket([]byte("JWNIT")).Put([]byte(urlEntry.Slug), encoded)
 		if err != nil {
 			return fmt.Errorf("could not insert URLEntry: %v", err)
 		}
 		return nil
 	})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H {
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H {
+			"data": urlEntry,
+			"error": "",
+		})
+	}
 	fmt.Println("Added URL Entry")
-	return err
 }
 
--- routes/jwnit_routes.go
+++ routes/jwnit_routes.go
@@ -1,51 +1,13 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/shayd3/jwn-it/controllers"
-	"github.com/shayd3/jwn-it/data"
 )
 
 func addJwnItRoutes(rg *gin.RouterGroup) {
 	jwnit := rg.Group("/")
 
-	jwnit.POST("/create", func(c *gin.Context) {
-		// urlEntry := URLEntry{}
-		// err := c.BindJSON(&urlEntry)
-		// urlEntry.Created = time.Now()
-		
-		// if err != nil {
-		// 	c.JSON(http.StatusBadRequest, gin.H {
-		// 		"error": err.Error(),
-		// 	})
-		// } else {
-		// 	c.JSON(http.StatusOK, gin.H {
-		// 		"data": urlEntry,
-		// 		"error": "",
-		// 	})
-		// }
-
-		// err = addURLEntry(db, urlEntry)
-		// if err != nil {
-		// 	return 
-		// }
-	})
-
-	jwnit.GET("/urls", func(c *gin.Context) {
-		fmt.Print(c.Keys)
-		urlEntries, err := controllers.GetURLEntries(data.DB)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H {
-				"error": err.Error(),
-			})
-		} else {
-			c.JSON(http.StatusOK, gin.H {
-				"data": urlEntries,
-				"error": "",
-			})
-		}
-	})
+	jwnit.POST("/create", controllers.AddURLEntry)
+	jwnit.GET("/urls", controllers.GetURLEntries)
 }
